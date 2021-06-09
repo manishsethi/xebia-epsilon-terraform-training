347d7226178441b6b6722d8e677e9deb17e2b612
manishsethi <manish@sethis.in>
2021-06-08 21:09:37 -0700

    Added data block

--- training-ec2/main.tf
+++ training-ec2/main.tf
@@ -2,7 +2,37 @@ provider "aws"{
     region = var.aws_region
 }
 
-resource "aws_instance" "xebia"{
-    ami            = var.ec2_ami
+data "aws_ami" "amazon_linux" {
+    most_recent = true
+
+    filter {
+        name = "name"
+        values = ["amzn2-ami-hvm-2.0.20210427.0-x86_64*"]
+    }
+
+    owners = ["137112412989"]
+} 
+
+resource "aws_ebs_volume" "add_volume" {
+    availability_zone = "us-east-1d"
+    size  = 50
+    tags = {
+       Name =  "${var.ec2_name}-volume1"
+    }
+}
+
+resource "aws_volume_attachment" "ebs_ec2_attachment" {
+    device_name  = "/dev/sdh"
+    volume_id    = aws_ebs_volume.add_volume.id
+    instance_id  = aws_instance.xebia.id
+}
+
+resource "aws_instance" "xebia" {
+    ami            = data.aws_ami.amazon_linux.id
     instance_type  = var.ec2_instancetype
+    subnet_id      = var.subnet_id
+    availability_zone = var.ec2_az
+    tags = {
+        Name  = var.ec2_name
+    }
 } 
--- training-ec2/variables.tf
+++ training-ec2/variables.tf
@@ -2,10 +2,22 @@ variable "aws_region" {
     default = "us-east-1"
 }
 
+variable "ec2_name" {
+    default = "Manish-Xebia-vm"
+}
+
 variable "ec2_ami" {
     default = "ami-0d5eff06f840b45e9"
 }
 
 variable "ec2_instancetype" {
     default = "t2.micro"
+}
+
+variable "subnet_id" {
+    default = "subnet-540e6632"
+}
+
+variable "ec2_az" {
+    default = "us-east-1d"
 }
