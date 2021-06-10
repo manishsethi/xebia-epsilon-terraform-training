12a232884dadbbf0612e6ca12c7490e67a41595b
manishsethi <manish@sethis.in>
2021-06-09 20:41:07 -0700

    added the provisioner

---
+++ modules/training-ansible/main.tf
@@ -0,0 +1,53 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+
+  backend "s3" {
+    bucket = "xebia-bucket-trainer-bucket"
+    key    = "terraform-ansible.tfstate"
+    region = "us-east-1"
+  }
+
+}
+
+provider "aws" {
+  region = var.aws_region
+}
+
+
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
+
+  owners = ["099720109477"]
+}
+
+resource "aws_instance" "web" {
+  count         = 1
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t2.micro"
+  key_name      = "ms-manish"
+
+  provisioner "remote-exec" {
+    inline = ["sudo apt update", "sudo apt install python3 -y", "sudo apt install apache2 -y", "echo Done!"]
+
+    connection {
+      host        = self.public_ip
+      type        = "ssh"
+      user        = "ubuntu"
+      private_key = file(var.pvt_key)
+    }
+  }
+
+#   provisioner "local-exec" {
+#     command = "ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -u ubuntu -i '${self.public_ip}' --private-key ${var.pvt_key} apache-install.yml"
+#   }
+}
